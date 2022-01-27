cb9d5ad35de695150d8f556c77b0055b5660a932
Hao-Ting Wang <htwangtw@gmail.com>
2022-01-27 11:00:14 -0500

    format stdout

--- mapsmasker_benchmark/main.py
+++ mapsmasker_benchmark/main.py
@@ -69,5 +69,6 @@ if __name__ == '__main__':
                 cur_r2[strategy] = r2
             r2_collector.append(cur_r2)
         r2_collector = pd.DataFrame(r2_collector)
+        print("correlation betwee the R2 of two strategies.")
         print(r2_collector.corr())
         print(f'=====================================')
