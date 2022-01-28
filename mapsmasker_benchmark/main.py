150fd4f9e1053167925814b62c67999ae69640bd
Hao-Ting Wang <htwangtw@gmail.com>
2022-01-28 13:22:24 -0500

    revise the r2 calculation

--- mapsmasker_benchmark/main.py
+++ mapsmasker_benchmark/main.py
@@ -4,15 +4,16 @@ import warnings
 warnings.filterwarnings(action='ignore')
 
 from pathlib import Path
-from nilearn.maskers import NiftiMapsMasker, NiftiLabelsMasker
-from nilearn.datasets import fetch_atlas_difumo, fetch_adhd, fetch_development_fmri
-from nilearn.image import index_img, binarize_img
+from nilearn.maskers import NiftiMapsMasker, NiftiMasker
+from nilearn.datasets import fetch_atlas_difumo, fetch_adhd
+from nilearn.image import load_img, math_img, mean_img, binarize_img
+from nilearn.plotting import view_img
 import numpy as np
-import pandas as pd
+import nibabel as nb
 from time import time
 
 
-DIFUMO_DIM = [64, 128, 256]
+DIFUMO_DIM = [64, 128, 256, 512, 1024]
 
 
 def timer_func(func):
@@ -26,49 +27,40 @@ def timer_func(func):
 
 
 if __name__ == '__main__':
-
+    results = Path(__file__).parents[1] / 'results'
     dataset = fetch_adhd(1, data_dir=Path(__file__).parents[1] / 'data')
+    func = load_img(dataset.func[0])
+    conf = dataset.confounds[0]
+    mask = binarize_img(mean_img(func))
+    # standardise original data
+    preprocessor = NiftiMasker(standardize=True, detrend=True)
+    data = preprocessor.inverse_transform(preprocessor.fit_transform(func, conf))
+
 
     @timer_func
     def mask_adhd_difumo(strategy, maps):
-        masker = NiftiMapsMasker(maps, strategy=strategy, detrend=True)
-        return masker.fit_transform(dataset.func[0], confounds=dataset.confounds[0])
+        masker = NiftiMapsMasker(maps, strategy=strategy,
+                                 standardize=True, detrend=True,
+                                 resampling_target='data')
+        timeseries = masker.fit_transform(func, conf)
+        return timeseries, masker
 
 
     for dim in DIFUMO_DIM:
         print(f'================={dim}=================')
 
-        atals = fetch_atlas_difumo(dimension=dim,
+        atlas = fetch_atlas_difumo(dimension=dim,
                                 resolution_mm=3,
                                 data_dir=Path(__file__).parents[1] / 'data')
-
+        r2_scores = {}
         for strategy in ['ridge', 'ols']:
-            print('===============================')
             print(strategy)
             print(f"Time to extract difumo {dim} dimensions:")
-            mask_adhd_difumo(strategy, atals.maps)
-            print("")
-
-        print('===============================')
-        print(f"...get R2 for each dimension")
-        r2_collector = []
-        for i in range(dim):
-            cur_dim = index_img(atals.maps, i)
-            cur_dim_bin = binarize_img(cur_dim)
-
-            masker = NiftiLabelsMasker(cur_dim_bin, strategy='mean', detrend=True)
-            original = masker.fit_transform(dataset.func[0],
-                                            confounds=dataset.confounds[0]).flatten()
-
-            cur_r2 = {}
-            for strategy in ['ridge', 'ols']:
-                soft_masker = NiftiMapsMasker(cur_dim, strategy=strategy, detrend=True)
-                soft_mask = soft_masker.fit_transform(dataset.func[0],
-                                                    confounds=dataset.confounds[0]).flatten()
-                r2 = np.corrcoef(original, soft_mask)[0, 1]**2
-                cur_r2[strategy] = r2
-            r2_collector.append(cur_r2)
-        r2_collector = pd.DataFrame(r2_collector)
-        print("correlation betwee the R2 of two strategies.")
-        print(r2_collector.corr())
-        print(f'=====================================')
+            timeseries, masker = mask_adhd_difumo(strategy, atlas.maps)
+            compressed = masker.inverse_transform(timeseries)
+            r2 = math_img("1 - np.var(data - compressed, axis=-1)", data=data, compressed=compressed)
+            r2 = math_img("mask * img", mask=mask, img=r2)
+            r2_scores[strategy] = [r2, compressed]
+            view = view_img(r2)
+            view.save_as_html(str(results / f'extraction-{strategy}_difumo-{dim}.html'))
+    print(f'=====================================')
